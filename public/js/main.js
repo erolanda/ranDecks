30f01cd3848039675a0fb890996c727726a12525
erolanda <elmaildemlanda@gmail.com>
2017-05-09 14:31:17 -0500

    show average elixir cost

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,14 @@ function generateDeck(){
       url = "http://www.clashapi.xyz/images/cards/" + value.idName + ".png";
       document.getElementById("i"+index).src = url;
     });
+	var totalCost = data.map(function(card){
+	  return card.elixirCost;
+	}).reduce(getSum);
+	var averageCost = Math.round(totalCost / (data.length) * 10) / 10;
+  $('#averageCost').html("<span class='average-cost'>Average Elixir Cost: " + averageCost + '</span>');
   });
+}
+
+function getSum(total, num) {
+  return total + num;
 }
